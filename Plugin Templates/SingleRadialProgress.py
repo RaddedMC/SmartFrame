72f75e10107827fec4b2bfac1fdbe7dbcfe5ff5c
RaddedMC <mycooljames@hotmail.com>
2021-05-24 21:59:09 -0400

    New template! Radial progress

--- Plugin Templates/SingleRadialProgress.py
+++ Plugin Templates/SingleRadialProgress.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 # RaddedMC's SmartFrame v2 -- FullCustomTemplate.py
 # This is a plugin template for SmartFrame v2.
-# This particular template is fully custom -- you can set your output to whatever image or tile size you'd like.
+# This particular template will let you set a percentage value to be rendered into a circle and some text.
 
 # Required deps: Pillow, termcolor
 
@@ -9,13 +9,12 @@
 # Assume you have root priveleges.
 # Use the variables in GenerateCard() to change the tile size and pixel density of your Card.
 # If you need additional files, place them into a folder of the same name as your plugin.
-# The module will grab the user's colors before running your code. This will be located in COLORS
-# The user's font is located in SMARTFRAMEFOLDER + "/Fonts/font1.ttf".
 # Use the global variable SMARTFRAMEFOLDER for a string with the location of the SmartFrame folder.
 # For debug/overflow purposes, make sure you set alttext to something that accurately represents your collected data.
 # Use printC(text, color of text) if you need to print. 
 
-# Edit the PIL 'image' variable in GenerateCard in any way that you like! The end result of the variable will be what appears in SmartFrame.
+# Make sure to change the sourcename, progress, alttext, and maintext variables!
+# progress should be a float out of 1.
 # If you return set all variables to None (ex, if data can't be found), SmartFrame will display nothing for this Card.
 
 # To test, just run your card in a terminal! The image will appear in your Smartframe/Cards folder. I recommend deleting this file before running SmartFrame again.
@@ -34,20 +33,54 @@ SMARTFRAMEFOLDER = ""
 COLORS = []
 
 #### YOUR CODE HERE ####
-def GenerateCard():
-    tilesX = 4 # Change this to change tile size
-    tilesY = 2 # Change this to change tile size
-    dpifactor = 200 # Change this to increase card resolution. Don't go too high!!!
-    imageresx = tilesX*dpifactor
-    imageresy = tilesY*dpifactor
-    image = Image.new("RGB", (tilesX*dpifactor, tilesY*dpifactor))
-    alttext = ""
-    imagedraw = ImageDraw.Draw(image)                 
+def GetCardData():
+    progress = 0.02
+    maintext = "Some data"
+    alttext = "Whatever you want!"
     
     # Your code here
     
-    return image, alttext, tilesX, tilesY
+    return progress, maintext, alttext
 #### YOUR CODE HERE ####
+
+def GenerateCard():
+    # EDIT THESE TO CUSTOMIZE YOUR PLUGIN'S APPEARANCE!
+    tilesX = 2 # I don't recommend changing these values for this template
+    tilesY = 2 # I don't recommend changing these values for this template
+    dpifactor = 200 # Change this to increase card resolution. Don't go too high!!!
+    backgroundcolor = COLORS[3] # Change this to a 3-value tuple (255, 200, 100) to change the background colour!
+    textcolor = COLORS[1] # Change this to a 3-value tuple to change the text colour!
+    progressfillcolor = (COLORS[3][0]+50, COLORS[3][1]+50, COLORS[3][2]+50) # Change this to a 3-value tuple to change the color of your progress meter!
+    printC("Progress bar fill color is " + str(progressfillcolor))
+    progressbgcolor = (COLORS[3][0]-50, COLORS[3][1]-50, COLORS[3][2]-50) # Change this to a 3-value tuple to change the background color of your progress meter!
+    printC("Progress bar background color is " + str(progressbgcolor))
+    
+    imageresx = tilesX*dpifactor
+    imageresy = tilesY*dpifactor
+    image = Image.new("RGB", (tilesX*dpifactor, tilesY*dpifactor))
+    imagedraw = ImageDraw.Draw(image)                 
+    imagedraw.rectangle([(0,0), (imageresx, imageresy)], fill=backgroundcolor)
+    
+    progress, maintext, alttext = GetCardData()
+    
+    if maintext and alttext:
+        maintextfont = ImageFont.truetype(SMARTFRAMEFOLDER + "/Fonts/font1.ttf", 15*round(dpifactor/50))
+        if progress < 0.1:
+            progresstextfont = ImageFont.truetype(SMARTFRAMEFOLDER + "/Fonts/font1.ttf", 30*round(dpifactor/50))
+            progresstexttop = (imageresy/4)-(dpifactor/10)
+        else:
+            progresstextfont = ImageFont.truetype(SMARTFRAMEFOLDER + "/Fonts/font1.ttf", 15*round(dpifactor/50))
+            progresstexttop = 5*imageresy/16
+        imagedraw.text((dpifactor/50,3*imageresy/4), maintext, font=maintextfont, fill=textcolor)
+        circlepos = [(imageresx/8,(imageresy/8)-(dpifactor/10)),(7*imageresx/8, (7*imageresy/8)-(dpifactor/10))]
+        imagedraw.arc(circlepos, start=135, end=45, fill=progressbgcolor, width=round(dpifactor/4)) # Background
+        imagedraw.arc(circlepos, start=135, end=(270*progress)+135, fill=progressfillcolor, width=round(dpifactor/4)) # Background
+        imagedraw.text(((imageresx/4)+(dpifactor/10), progresstexttop), str(round(progress*100))+"%", font=progresstextfont, fill=textcolor)
+    else:
+        printC("No data! Sending null data.", "red")
+        return None, None, None, None
+    
+    return image, alttext, tilesX, tilesY
 
 
 
