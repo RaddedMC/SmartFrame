18eb3650a193ac3ed083ff06fd5f27d0145c43fb
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-05-27 22:04:05 -0400

    Updated dependencies requirement comments

--- Available Plugins/NewCovidCasesOntario.py
+++ Available Plugins/NewCovidCasesOntario.py
@@ -4,7 +4,7 @@
 # Ontario COVID data from the Government of Ontario.
 # This particular template will let you show a single large number and some small text with a fancy background.
 
-# Required deps for NewCasesOntarioCovid: Pillow, termcolor, requests.
+# Required deps for NewCasesOntarioCovid: Pillow, termcolor, requests, csv, datetime
 
 # No need to define any user variables! It just works (hopefully). 
 
