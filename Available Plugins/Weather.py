0e76cda87e0d1c97059835a4f56961fd4c5f9be2
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-05-28 16:10:21 -0400

    lol typo

--- Available Plugins/Weather.py
+++ Available Plugins/Weather.py
@@ -9,7 +9,7 @@
 # Declare your variables here (e.g. location to get weather data for)
 sourcename = "Weather"
 
-owm_api_key = "825400b6f9c8ad02b637fa732cdce498" # up to 60 requests/minute. Feel free to use it for now. I might remove it in the futre. If that's the case, register for your own API key at OpenWeatherMap and replace yours here!
+owm_api_key = "825400b6f9c8ad02b637fa732cdce498" # up to 60 requests/minute. Feel free to use it for now. I might remove it in the futre. If that's the case, register for your own API key at OpenWeatherMap and replace this one with yours here!
 city = "" # Your city/town goes here!
 country_code = "" # Must be ISO 3166-2 code (Tip: Google the ISO 3166-2 for your country)
 unit = "" # metric or imperial only (is cap-sensitive, default is set to metric) (e.g. unit = "metric")
