ca72d50c02d89a92368d127ded5f12db37523ab4
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-05-28 16:06:14 -0400

    Fixed description for dep requirements

--- Available Plugins/NewCovidCasesOntario.py
+++ Available Plugins/NewCovidCasesOntario.py
@@ -4,9 +4,9 @@
 # Ontario COVID data from the Government of Ontario.
 # This particular template will let you show a single large number and some small text with a fancy background.
 
-# Required deps for NewCasesOntarioCovid: Pillow, termcolor, requests, csv, datetime.
+# Required deps for NewCasesOntarioCovid: Pillow, termcolor, requests, datetime (csv is included in python!).
 
-# No need to define any user variables! It just works. Unless, you want to get rid of the time logic (see below).
+# No need to define any user variables! It just works. 
 
 # Add any user-definable variables here! (API keys, usernames, etc.)
 sourcename = "NewCovidCasesOntario"
